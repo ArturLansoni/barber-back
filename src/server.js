c0507f6a2db6aef9b19114331f485dbef4d311dc
Artur Lansoni <arturlansoni@gmail.com>
2021-05-10 19:18:06 -0300

    refactor: server.js

--- src/server.js
+++ src/server.js
@@ -1,59 +1,14 @@
-const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const employees = require("./model");
-const router = express.Router();
+const setupRouter = require("./routes");
+const setupDatabase = require("./database");
 const port = 4000;
 
-const uri = "mongodb://localhost:27017/kennel";
-
-mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
-
-const connection = mongoose.connection;
-
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
-
-router.route("/insert").post((req, res) => {
-  const data = [
-    {
-      name: "John",
-      age: 21,
-      location: "New York",
-    },
-    {
-      name: "Smith",
-      age: 27,
-      location: "Texas",
-    },
-    {
-      name: "Lisa",
-      age: 23,
-      location: "Chicago",
-    },
-  ];
-
-  employees.insertMany(data, (err, result) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-router.route("/").get((req, res) => {
-  employees.find({}, (err, result) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-app.use("/", router);
+setupDatabase();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+setupRouter(app);
 
 app.listen(port, () => {
   console.log("Server is running on Port: " + port);

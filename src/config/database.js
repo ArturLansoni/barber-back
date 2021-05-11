e8a579e8c1febbaa5668d386448852b5d68fa53c
Artur Lansoni <arturlansoni@gmail.com>
2021-05-11 20:14:47 -0300

    feat: add database logs

--- src/config/database.js
+++ src/config/database.js
@@ -7,9 +7,13 @@ module.exports = () => {
     useNewUrlParser: true,
   });
 
-  const connection = mongoose.connection;
-
-  connection.once("open", () => {
+  mongoose.connection.once("open", () => {
     console.log("MongoDB database connection established successfully");
+  });
+  mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose! Disconnected");
+  });
+  mongoose.connection.on("error", () => {
+    console.log("Mongoose! Error at the connection");
   });
 };

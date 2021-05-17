9ae108e041e733cf3f50b94dc78550b30cc99b36
Artur Lansoni <arturlansoni@gmail.com>
2021-05-17 19:18:14 -0300

    feat: add jwtSecret to env file

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT || 4000,
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/barber",
+  jwtSecret: process.env.JWT_SECRET || "^u:]v!kJ=py8Z?aR3tIf?(_wA([qHM",
 };
